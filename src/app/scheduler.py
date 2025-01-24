4ae63b0003b6389fde4d807fa6d6c49aa5ea5a8e
Irina Ivashko <ira.ivashko.99@gmail.com>
2025-01-24 16:42:49 +0300

    Удаление ненужных комментариев

--- src/app/scheduler.py
+++ src/app/scheduler.py
@@ -1,10 +1,6 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 import logging
 logging.basicConfig(level=logging.INFO)
-# job_stores = {
-#     'default': SQLAlchemyJobStore(url="sqlite:///./jobs.sqlite")
-# }
 async_scheduler = AsyncIOScheduler()
 async_scheduler.add_jobstore('sqlalchemy', url='sqlite:///./jobs.sqlite')
 
